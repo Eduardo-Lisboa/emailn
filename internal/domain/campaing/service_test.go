a6c61a99f01654dd39e43b2715c07f692d46350e
Eduardo Henrique Lisboa Alves <henriqueeduardo63@gmail.com>
2024-01-18 22:55:06 -0300

    Refactor Create method in Campaign service
    Add validation for domain error in Create method
    
    Add test for validating repository save in Create method
    
    ep77

--- internal/domain/campaing/service_test.go
+++ internal/domain/campaing/service_test.go
@@ -2,10 +2,20 @@ package campaing
 
 import (
 	"emailn/internal/contract"
+	"errors"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
+)
+
+var (
+	newCampaing = contract.NewCampaing{
+		Name:    "Teste",
+		Content: "Body",
+		Emails:  []string{"Teste@gmial.com"},
+	}
+	service = Service{}
 )
 
 type repositoryMock struct {
@@ -19,26 +29,22 @@ func (r *repositoryMock) Save(campaing *Campaing) error {
 
 func Test_Create_Campaig(t *testing.T) {
 	assert := assert.New(t)
-	service := Service{}
-	newCampaing := contract.NewCampaing{
-		Name:    "Teste",
-		Content: "Body",
-		Emails:  []string{"Teste@gmial.com"},
-	}
-
 	id, err := service.Create(newCampaing)
-
 	assert.Nil(err)
 	assert.NotNil(id)
 
 }
 
+func Test_Create_ValidateDomanError(t *testing.T) {
+	assert := assert.New(t)
+	newCampaing.Name = ""
+	_, err := service.Create(newCampaing)
+	assert.NotNil(err)
+	assert.Equal("name is required", err.Error())
+
+}
+
 func Test_Create_SaveCampaing(t *testing.T) {
-	newCampaing := contract.NewCampaing{
-		Name:    "Teste",
-		Content: "Body",
-		Emails:  []string{"Teste@gmial.com"},
-	}
 	repositoryMock := new(repositoryMock)
 	repositoryMock.On("Save", mock.MatchedBy(func(campaing *Campaing) bool {
 		if campaing.Name != newCampaing.Name {
@@ -50,8 +56,19 @@ func Test_Create_SaveCampaing(t *testing.T) {
 		}
 		return true
 	})).Return(nil)
-	service := Service{Repository: repositoryMock}
-	service.Create(newCampaing)
 
+	service.Repository = repositoryMock
+	service.Create(newCampaing)
 	repositoryMock.AssertExpectations(t)
+}
+
+func Test_Create_ValidateRepositorySave(t *testing.T) {
+	assert := assert.New(t)
+	repositoryMock := new(repositoryMock)
+	repositoryMock.On("Save", mock.Anything).Return(errors.New("error to save on database"))
+
+	service.Repository = repositoryMock
+	_, err := service.Create(newCampaing)
+
+	assert.Equal("error to save on database", err.Error())
 }
